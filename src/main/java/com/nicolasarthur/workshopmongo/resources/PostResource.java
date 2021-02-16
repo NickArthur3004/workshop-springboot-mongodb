3b0312b8212460d2c16d855d58ee56f729b5b340
NickArthur3004 <nicolas.arthur2786@gmail.com>
2021-02-16 12:22:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/nicolasarthur/workshopmongo/resources/PostResource.java
@@ -0,0 +1,28 @@
+package com.nicolasarthur.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nicolasarthur.workshopmongo.domain.Post;
+import com.nicolasarthur.workshopmongo.domain.User;
+import com.nicolasarthur.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/nicolasarthur/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.nicolasarthur.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nicolasarthur.workshopmongo.domain.Post;
+import com.nicolasarthur.workshopmongo.domain.User;
+import com.nicolasarthur.workshopmongo.dto.UserDTO;
+import com.nicolasarthur.workshopmongo.repository.PostRepository;
+import com.nicolasarthur.workshopmongo.repository.UserRepository;
+import com.nicolasarthur.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
